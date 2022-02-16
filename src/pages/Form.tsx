8dbd9d3c68cdc8cf28243badfc05462edd2bea0c
Kareem Arafa <kareem.arafa@afaqy.com>
2022-02-17 00:24:58 +0200

    WIP create dynamic UI form with metadata array and data object

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,6 +1,43 @@
 const Form = () => {
+  const metadata: any = {
+    fields: [
+      {
+        id: 'name',
+        type: 'text',
+        label: 'Name'
+      },
+      {
+        id: 'age',
+        type: 'number',
+        label: 'Age'
+      },
+      {
+        id: 'comment',
+        type: 'textarea',
+        label: 'Comment',
+        disabled: true
+      }
+    ]
+  }
+
+  const data: any = {
+    name: "Bob",
+    age: 42,
+    comment: "The best developer in the world"
+  }
+
   return (
-    <h1>Hello!</h1>
+    <form>
+      {Object.keys(data).map((key: any) => {
+        const fieldMetadata = metadata.fields.filter((meta: any) => meta.id === key)[0];
+        return (
+          <div key={fieldMetadata.id}>
+            <label className="form-label" htmlFor={fieldMetadata.id}>{fieldMetadata.label}</label>
+            <input id={fieldMetadata.id} className="form-control" type={fieldMetadata.type}/>
+          </div>
+        )
+      })}
+    </form>
   )
 }
 export default Form;
