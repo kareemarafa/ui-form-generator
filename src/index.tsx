90209c6939b06b6322fdd87ce058cc3cc261b16b
Kareem Arafa <kareem.arafa@afaqy.com>
2022-02-16 23:30:05 +0200

    add boostrap and override.scss to make proper customizations

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import './styles/override.scss';
+
+import {Layout} from "./pages/Layout";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Layout />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/Layout.tsx
@@ -0,0 +1,13 @@
+export const Layout = () => {
+  return (
+    <>
+      <div className="container pt-2">
+        <div className="row">
+          <div className="col">
+            <button className="btn btn-primary">Show here</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
