077075c0692ae3c768bff88a109fbeb197f93f74
Kareem Arafa <kareem.arafa@afaqy.com>
2022-02-19 22:59:17 +0200

    add not found route

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,6 +1,6 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import GeneratedForm from "./GeneratedForm";
 import FormOneData from '../data/FormOneData';
 import FormTwoData from '../data/FormTwoData';
@@ -9,15 +9,32 @@ import FormThreeData from '../data/FormThreeData';
 const Layout = () => {
   return (
     <>
-      <Navbar />
+      <Navbar/>
       <div className="container pt-3 mb-5">
         <Routes>
-          <Route path="/" element={<GeneratedForm data={FormOneData.data} metadata={FormOneData.metadata} />} />
-          <Route path="/product" element={<GeneratedForm data={FormTwoData.data} metadata={FormTwoData.metadata} />} />
-          <Route path="/listing" element={<GeneratedForm data={FormThreeData.data} metadata={FormThreeData.metadata} />} />
+          <Route path="/" key={1} element={
+            <GeneratedForm data={FormOneData.data}
+                           metadata={FormOneData.metadata}/>
+          }/>
+          <Route path="/product" key={2} element={
+            <GeneratedForm data={FormTwoData.data}
+                           metadata={FormTwoData.metadata}/>
+          }/>
+          <Route path="/listing" key={3} element={
+            <GeneratedForm data={FormThreeData.data}
+                           metadata={FormThreeData.metadata}
+            />}/>
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
         </Routes>
       </div>
-      <Footer />
+      <Footer/>
     </>
   )
 }
