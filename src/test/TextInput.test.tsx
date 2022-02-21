6b0faac0ad733ef1d9bbdcb995a08a66fdb52a6d
Kareem Arafa <kareem.arafa@afaqy.com>
2022-02-21 03:19:13 +0200

    Add Test cases in comment at TextInput.test.tsx

--- src/test/TextInput.test.tsx
+++ src/test/TextInput.test.tsx
@@ -22,7 +22,8 @@ test('Render TextInput with type text', () => {
   const metadataSeed: Metadata = {fields: [{id: 'name', type: 'text', label: 'Name'}]};
   const dataSeed = {name: 'Bob'};
   renderWithRouter(<GeneratedForm data={dataSeed} metadata={metadataSeed}/>, {route});
-  expect(screen.getByTestId('name')).toBeDefined()
+  const inputControl = screen.getByTestId('name') as HTMLInputElement;
+  expect(inputControl.value).toEqual('Bob')
 });
 
 test('Render TextInput with type number', () => {
@@ -30,14 +31,14 @@ test('Render TextInput with type number', () => {
   const metadataSeed: Metadata = {fields: [{id: 'age', type: 'number', label: 'Age'}]};
   const dataSeed = {age: 42};
   renderWithRouter(<GeneratedForm data={dataSeed} metadata={metadataSeed}/>, {route});
-  expect(screen.getByTestId('age')).toBeDefined()
-});
+  const inputControl = screen.getByTestId('age') as HTMLInputElement;
+  expect(inputControl.value).toEqual('42')});
 
 test('Render TextInput with type email', () => {
   const route = '/some-route';
   const metadataSeed: Metadata = {fields: [{id: 'email', type: 'email', label: 'Email'}]};
   const dataSeed = {email: 'bob@app.com'};
   renderWithRouter(<GeneratedForm data={dataSeed} metadata={metadataSeed}/>, {route});
-  expect(screen.getByTestId('email')).toBeDefined()
-});
+  const inputControl = screen.getByTestId('email') as HTMLInputElement;
+  expect(inputControl.value).toEqual('bob@app.com')});
 
